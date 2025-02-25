30147e54e4744949f53fb7ff42cc5b61d2168843
edgaremy <remy.edgar@gmail.com>
2025-02-25 18:10:11 +0100

    Added lines to example

--- samples/python.py
+++ samples/python.py
@@ -1,4 +1,5 @@
 import math
+import os
 
 # A very simple function
 def square(number):
@@ -14,6 +15,12 @@ def main():
     for i in range(10):
         print(f"The square of {i} is:
               {math.pow(user_input, 2)(i)}")
+    
+    # A simple condition
+    if os.path.exists("samples"):
+        print("The directory 'samples' exists") # TODO
+    else:
+        print("The directory 'samples' does not exist")
 
 if __name__ == "__main__":
     main()
